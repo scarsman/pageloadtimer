75057b1f91b422e1adfc19db8888f6c557b6f423
Perpo Hipolito <poohsna@gmail.com>
2017-12-12 17:19:23 +0800

    Update pageloadtimer.py

--- pageloadtimer.py
+++ pageloadtimer.py
@@ -61,8 +61,32 @@ class PageLoadTimer:
 
 if __name__ == '__main__':
     with Xvfb() as xvfb:
-        url = 'http://www.example.com'
+        url = 'http://google.com'
         driver = webdriver.Firefox()
+        #driver = webdriver.Chrome()
+        #driver = webdriver.PhantomJS()
         driver.get(url)
         timer = PageLoadTimer(driver)
-        print timer.get_event_times()
+        events_time = timer.get_event_times()
+        
+        total_page_load_time = ""
+        total_request_respond_time = ""
+	total_page_render_time = ""
+	
+	print "** Events Time"
+	print "----------------------------"
+	for key in events_time.iterkeys():
+
+		print "%s: %s" % (key, events_time[key])
+
+		total_page_load_time = (events_time["loadEventEnd"] - events_time["navigationStart"])
+		total_request_respond_time = (events_time["responseEnd"] - events_time["requestStart"])
+		total_page_render_time = (events_time["domComplete"] - events_time["domLoading"])
+
+	print "-----------------------------"
+		
+	print "Total Page Load Time: %s ms" % total_page_load_time 
+	print "Total Request Respond Time: %s ms" % total_request_respond_time
+	print "Total Page Render Time: %s ms" % total_page_render_time
+
+	
